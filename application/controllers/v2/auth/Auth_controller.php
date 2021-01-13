eaf354b2db04efd2d992b870a4c7b2cc53b8f7f1
Aleck Mattos <aleckyann@gmail.com>
2021-01-13 17:01:23 -0300

    redirect login

--- application/controllers/v2/auth/Auth_controller.php
+++ application/controllers/v2/auth/Auth_controller.php
@@ -26,7 +26,7 @@ class Auth_controller extends CI_Controller
         if (!empty($usuario_autenticado)) {
             $this->session->set_userdata($usuario_autenticado);
             $this->session->set_flashdata('info', '<i class="far fa-smile-beam"></i> SEJA BEM VINDO AO INFOSUS!');
-            redirect('v2/pacientes/listagem');
+            redirect('v2/dashboard');
         } else {
             $this->session->set_flashdata('danger', 'Login ou senha incorretos.');
             redirect();

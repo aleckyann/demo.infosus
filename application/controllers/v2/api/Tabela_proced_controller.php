89f1ac66b7d1b101b8c1253464c1889fbb268d7e
Aleck Mattos <aleckyann@gmail.com>
2021-01-30 18:21:02 -0300

    bugs corrigidos em procedimentos tabela proced select2

--- application/controllers/v2/api/Tabela_proced_controller.php
+++ application/controllers/v2/api/Tabela_proced_controller.php
@@ -8,7 +8,7 @@ class Tabela_proced_controller extends Sistema_Controller
     public function select2(): void
     {
         $nome = $this->input->post('nome');
-        $resultado = $this->db->select('nome id, nome text')->like(['nome' => $nome])->get('tabela_proced')->result_array();
+        $resultado = $this->db->select('id id, nome text')->like(['nome' => $nome])->get('tabela_proced')->result_array();
         $this->output
             ->set_content_type('application/json')
             ->set_output(

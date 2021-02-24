b86726df0e5690b4758aea5837ba7987da1cfeeb
Aleck Mattos <aleckyann@gmail.com>
2021-02-24 15:52:49 -0300

    correcao estabelecimentos

--- application/controllers/v2/api/Estabelecimentos_controller.php
+++ application/controllers/v2/api/Estabelecimentos_controller.php
@@ -10,34 +10,18 @@ class Estabelecimentos_controller extends Sistema_Controller
         $estabelecimento = $this->input->post('estabelecimento');
         $resultado = $this->db->select('estabelecimento_id id, estabelecimento_nome text')->like(['estabelecimento_nome' => $estabelecimento])->get_where('estabelecimentos',['estabelecimento_tipo'=>'SOLICITANTE'])->result_array();
 
-        if (count($resultado) == 1) {
-            $this->output
-            ->set_content_type('application/json')
-            ->set_output(json_encode($resultado[0]));
-        } elseif (count($resultado) > 1) {
-            $this->output
-            ->set_content_type('application/json')
-            ->set_output(json_encode($resultado));
-        } else {
-            show_404();
-        }
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($resultado));
     }
 
     public function prestadores(): void
     {
         $estabelecimento = $this->input->post('estabelecimento');
         $resultado = $this->db->select('estabelecimento_id id, estabelecimento_nome text')->like(['estabelecimento_nome' => $estabelecimento])->get_where('estabelecimentos', ['estabelecimento_tipo' => 'PRESTADOR'])->result_array();
-
-        if (count($resultado) == 1) {
-            $this->output
-            ->set_content_type('application/json')
-            ->set_output(json_encode($resultado[0]));
-        } elseif (count($resultado) > 1) {
-            $this->output
-            ->set_content_type('application/json')
-            ->set_output(json_encode($resultado));
-        } else {
-            show_404();
-        }
+        
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($resultado));
     }
 }

27874a09072d7f9068dab5c6eba8367213eb9a53
Aleck Mattos <aleckyann@gmail.com>
2021-03-05 18:18:34 -0300

    correcao no flashdata da pagina de login; mensagem agora aparece aparece;

--- application/controllers/v2/api/Tabela_proced_controller.php
+++ application/controllers/v2/api/Tabela_proced_controller.php
@@ -8,7 +8,7 @@ class Tabela_proced_controller extends Sistema_Controller
     public function select2(): void
     {
         $nome = $this->input->post('procedimento_nome');
-        $resultado = $this->db->select('id id, nome text')->like(['procedimento_nome' => $nome])->get('tabela_proced')->result_array();
+        $resultado = $this->db->select('id id, nome text')->like(['nome' => $nome])->get('tabela_proced')->result_array();
 
         $this->output
         ->set_content_type('application/json')

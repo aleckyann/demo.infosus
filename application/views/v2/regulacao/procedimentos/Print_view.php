6a300ff7ccd344f11a156d82a0e6587f6cd99bdc
Aleck Mattos <aleckyann@gmail.com>
2021-03-08 17:51:36 -0300

    first version print procediments

--- application/views/v2/regulacao/procedimentos/Print_view.php
+++ application/views/v2/regulacao/procedimentos/Print_view.php
@@ -2,12 +2,12 @@
 // Dados complementares temporários
 $estabelecimento_prestador = $this->db->get_where('estabelecimentos', ['estabelecimento_id' => $procedimento['estabelecimento_prestador']])->row_array();
 ?>
-<div class="d-flex mb-2">
+<div class="d-flex mb-2 d-print-none">
     <div class="card overflow-hidden flex-1">
         <div class="bg-holder bg-card" style="background-image:url(<?= base_url('public/v2/assets/img/illustrations/corner-4.png') ?>);"></div>
         <!--/.bg-holder-->
         <div class="card-body position-relative">
-            <a class="float-right btn d-print-none" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            <a class="float-right btn" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                 <i class="fas fa-question-circle"></i> <span class="font-weight-light">Ajuda</span>
             </a>
             <h4 class="font-weight-light">
@@ -26,72 +26,106 @@ $estabelecimento_prestador = $this->db->get_where('estabelecimentos', ['estabele
     </div>
 </div>
 
-
 <div class="card mb-3">
     <?= $this->ui->alert_flashdata() ?>
-
     <div class="card-body row">
         <h5 class="font-weight-bold">DADOS DO PACIENTE</h5>
-        <div class="col-9">
+        <div class="col-8">
             <label for="">Nome</label>
-            <input type="text" class="form-control" value="<?= $procedimento['nome_paciente'] ?>" readonly>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['nome_paciente'] ?>" readonly>
         </div>
-        <div class="col-3">
+        <div class="col-4">
             <label for="">Nascimento</label>
-            <input type="text" class="form-control" value="<?= date_format(date_create($procedimento['nascimento']), 'd/m/Y') ?>" readonly>
+            <input type="text" class="form-control pr-0" value="<?= date_format(date_create($procedimento['nascimento']), 'd/m/Y') ?>" readonly>
         </div>
-        <div class="col-9">
+        <div class="col-8">
             <label for="">Endereço</label>
-            <input type="text" class="form-control" value="<?= $procedimento['endereco_paciente'] ?>" readonly>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['endereco_paciente'] ?>" readonly>
+        </div>
+        <div class="col-4">
+            <label for="">Bairro</label>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['bairro_paciente'] ?>" readonly>
         </div>
         <div class="col-3">
             <label for="">Telefone</label>
-            <input type="text" class="form-control" value="<?= $procedimento['telefone_paciente'] ?>" readonly>
-        </div>
-
-        <div class="my-3"></div>
-
-        <h5 class="font-weight-bold">DADOS DO PROCEDIMENTO</h5>
-        <div class="col-9">
-            <label>Procedimento:</label>
-            <input class="form-control" value="<?= $procedimento['nome'] ?>" readonly>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['telefone_paciente'] ?>" readonly>
         </div>
         <div class="col-3">
-            <label>Urgência (1-4):</label>
-            <input class="form-control" value="<?= $procedimento['procedimento_risco'] ?>" readonly>
+            <label for="">CPF</label>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['cpf'] ?>" readonly>
         </div>
-        <div class="col-4">
-            <label>Cota utilizada:</label>
-            <input class="form-control" value="<?= $procedimento['cota'] ?>" readonly>
+        <div class="col-3">
+            <label for="">RG</label>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['identidade'] ?>" readonly>
         </div>
-        <div class="col-4">
-            <label>Data da solicitação:</label>
-            <input class="form-control" value="<?= date_format(date_create($procedimento['data_solicitacao']), 'd/m/Y') ?>" readonly>
+        <div class="col-3">
+            <label for="">CNS</label>
+            <input type="text" class="form-control pr-0" value="<?= $procedimento['cns_paciente'] ?>" readonly>
         </div>
-        <div class="col-4">
-            <label>Data do atendimento:</label>
-            <input class="form-control" value="<?= date_format(date_create($procedimento['data']), 'd/m/Y') ?>" readonly>
+
+        <div class="my-2"></div>
+
+        <h5 class="font-weight-bold">DADOS DA SOLICITAÇÃO</h5>
+        <div class="col-12">
+            <label>Nome do procedimento</label>
+            <input class="form-control" value="<?= $procedimento['nome'] ?>" readonly>
         </div>
         <div class="col-6">
-            <label>Profissional solicitante:</label>
+            <label>Especialidade</label>
+            <input class="form-control" value="<?= $procedimento['especialidade_nome'] ?>" readonly>
+        </div>
+        <div class="col-6">
+            <label>Profissional solicitante</label>
             <input class="form-control" value="<?= $procedimento['profissional_nome'] ?>" readonly>
         </div>
         <div class="col-6">
-            <label>Estabelecimento solicitante:</label>
+            <label>Estabelecimento solicitante</label>
             <input class="form-control" value="<?= $procedimento['estabelecimento_nome'] ?>" readonly>
         </div>
-        <div class="col-6">
-            <label>Estabelecimento prestador:</label>
-            <input class="form-control" value="<?= @$estabelecimento_prestador['estabelecimento_nome'] ?>" readonly>
+        <div class="col-3">
+            <label>Data da solicitação</label>
+            <input class="form-control" value="<?= date_format(date_create($procedimento['data_solicitacao']), 'd/m/Y') ?>" readonly>
         </div>
-        <div class="col-6">
-            <label>Cidade do prestador:</label>
-            <input class="form-control" value="<?= $procedimento['nome_municipio'] ?>" readonly>
+        <div class="col-3">
+            <label>Grau de urgência (1-4)</label>
+            <input class="form-control" value="<?= $procedimento['procedimento_risco'] ?>" readonly>
+        </div>
+        <div class="col-12">
+            <label>Sintomas/Observações</label>
+            <textarea class="form-control" readonly><?= $procedimento['procedimento_risco'] ?></textarea>
         </div>
 
-        <div class="card-footer bg-light mt-1">
+        <div class="my-2"></div>
+
+        <h5 class="font-weight-bold">DADOS DO AGENDAMENTO</h5>
+
+        <div class="col-4">
+            <label>Cidade do prestador</label>
+            <input class="form-control" value="<?= $procedimento['nome_municipio'] ?>" readonly>
+        </div>
+        <div class="col-4">
+            <label>Estabelecimento prestador</label>
+            <input class="form-control" value="<?= @$estabelecimento_prestador['estabelecimento_nome'] ?>" readonly>
+        </div>
+        <div class="col-4">
+            <label>Data do atendimento</label>
+            <input class="form-control" value="<?= date_format(date_create($procedimento['data']), 'd/m/Y') ?>" readonly>
+        </div>
+        <div class="col-12">
+            <label>Cota utilizada</label>
+            <input class="form-control" value="<?= $procedimento['cota'] ?>" readonly>
+        </div>
+
+        <div class="d-none d-print-block mt-5">
+            <div class="row">
+                <div class="col-6 text-center small"><u>_________________________</u> <br><?= $procedimento['nome_paciente']?></div>
+                <div class="col-6 text-center small"><u>_________________________</u> <br>Secretária Municipal de Saúde</div>
+            </div>
+        </div>
+
+        <div class="card-footer bg-light mt-2">
             <p class="text-center small"><strong>Documento gerado na data de </strong> <u><?= date('d/m/Y H:i:s') ?></u> <strong>em</strong> <u><?= base_url() ?></u></p>
         </div>
     </div>
 </div>
-    <button class="btn btn-primary font-weight-light d-print-none" onclick="window.print()"><i class="fas fa-print"></i> Imprimir página</button>
+<button class="btn btn-primary font-weight-light d-print-none" onclick="window.print()"><i class="fas fa-print"></i> Imprimir página</button>

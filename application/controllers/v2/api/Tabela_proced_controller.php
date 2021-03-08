4c7209e819bcb7cabb89d2f97022df7106d3d6d9
Aleck Mattos <aleckyann@gmail.com>
2021-03-08 09:43:45 -0300

    ok

--- application/controllers/v2/api/Tabela_proced_controller.php
+++ application/controllers/v2/api/Tabela_proced_controller.php
@@ -7,7 +7,7 @@ class Tabela_proced_controller extends Sistema_Controller
 
     public function select2(): void
     {
-        $nome = $this->input->post('procedimento_nome');
+        $nome = $this->input->post('nome');
         $resultado = $this->db->select('id id, nome text')->like(['nome' => $nome])->get('tabela_proced')->result_array();
 
         $this->output

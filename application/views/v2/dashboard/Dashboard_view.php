64668124919c666778f7228719f86454f00d5236
Aleck Mattos <aleckyann@gmail.com>
2021-03-24 08:41:35 -0300

    new teste

--- application/views/v2/dashboard/Dashboard_view.php
+++ application/views/v2/dashboard/Dashboard_view.php
@@ -8,14 +8,14 @@
             <div class="card-body">
                 <h6>Dashboards</h6>
                 <p class="font-weight-light my-0">
-                    Acesso rápido e de forma visual aos números do seu infosus
+                    Acesso rápido e de forma visual aos números de cada módulo do seu infosus.
                 <div class="btn-group float-right" role="group">
                     <button class="btn btn-light btn-sm dropdown-toggle" style="opacity:0.6" id="btnGroupVerticalDrop4" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dashboards</button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop4">
-                        <a class="dropdown-item" href="#">Dashboard 1</a>
-                        <a class="dropdown-item" href="#">Dashboard 2</a>
-                        <a class="dropdown-item" href="#">Dashboard 3</a>
-                        <a class="dropdown-item" href="#">Dashboard 4</a>
+                        <a class="dropdown-item" href="<?= base_url('v2/dashboard') ?>">Gestão</a>
+                        <a class="dropdown-item" href="<?= base_url('v2/dashboard') ?>">Regulação</a>
+                        <a class="dropdown-item" href="<?= base_url('v2/dashboard') ?>">Transportes</a>
+                        <a class="dropdown-item" href="<?= base_url('v2/dashboard') ?>">Almoxarifado</a>
                     </div>
                 </div>
                 </p>
@@ -28,14 +28,11 @@
             <div class="card-body">
                 <h6>Pacientes</h6>
                 <p class="font-weight-light my-0">
-                    Acesso rápido e de forma visual aos números do seu infosus
+                    Controle total e facilidade para adicionar, editar e visualizar históricos dos pacientes.
                 <div class="btn-group float-right" role="group">
                     <button class="btn btn-light btn-sm dropdown-toggle" style="opacity:0.6" id="btnGroupVerticalDrop4" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop4">
-                        <a class="dropdown-item" href="#">Dashboard 1</a>
-                        <a class="dropdown-item" href="#">Dashboard 2</a>
-                        <a class="dropdown-item" href="#">Dashboard 3</a>
-                        <a class="dropdown-item" href="#">Dashboard 4</a>
+                        <a class="dropdown-item" href="P">Listagem de pacientes</a>
                     </div>
                 </div>
                 </p>
@@ -48,14 +45,13 @@
             <div class="card-body">
                 <h6>Regulação</h6>
                 <p class="font-weight-light my-0">
-                    Acesso rápido e de forma visual aos números do seu infosus
+                    Controle todos os processos envolvidos nos Procedimentos, TFD e Casa de apoio.
                 <div class="btn-group float-right" role="group">
                     <button class="btn btn-light btn-sm dropdown-toggle" style="opacity:0.6" id="btnGroupVerticalDrop4" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Regulação</button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop4">
-                        <a class="dropdown-item" href="#">Dashboard 1</a>
-                        <a class="dropdown-item" href="#">Dashboard 2</a>
-                        <a class="dropdown-item" href="#">Dashboard 3</a>
-                        <a class="dropdown-item" href="#">Dashboard 4</a>
+                        <a class="dropdown-item" href="#">TFD</a>
+                        <a class="dropdown-item" href="#">Procedimentos</a>
+                        <a class="dropdown-item" href="#">Casa de apoio</a>
                     </div>
                 </div>
                 </p>
@@ -68,14 +64,12 @@
             <div class="card-body">
                 <h6>Transportes</h6>
                 <p class="font-weight-light my-0">
-                    Acesso rápido e de forma visual aos números do seu infosus
+                    Faça o controle dos veículos e gestão das viagens e passageiros.
                 <div class="btn-group float-right" role="group">
                     <button class="btn btn-light btn-sm dropdown-toggle" style="opacity:0.6" id="btnGroupVerticalDrop4" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Transportes</button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop4">
-                        <a class="dropdown-item" href="#">Dashboard 1</a>
-                        <a class="dropdown-item" href="#">Dashboard 2</a>
-                        <a class="dropdown-item" href="#">Dashboard 3</a>
-                        <a class="dropdown-item" href="#">Dashboard 4</a>
+                        <a class="dropdown-item" href="#">Veículos</a>
+                        <a class="dropdown-item" href="#">Viagens</a>
                     </div>
                 </div>
                 </p>
@@ -88,14 +82,12 @@
             <div class="card-body">
                 <h6>Almoxarifado</h6>
                 <p class="font-weight-light my-0">
-                    Acesso rápido e de forma visual aos números do seu infosus
+                    Realização de controle de estoques, produtos e almoxarifados.
                 <div class="btn-group float-right" role="group">
                     <button class="btn btn-light btn-sm dropdown-toggle" style="opacity:0.6" id="btnGroupVerticalDrop4" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Almoxarifado</button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop4">
-                        <a class="dropdown-item" href="#">Dashboard 1</a>
-                        <a class="dropdown-item" href="#">Dashboard 2</a>
-                        <a class="dropdown-item" href="#">Dashboard 3</a>
-                        <a class="dropdown-item" href="#">Dashboard 4</a>
+                        <a class="dropdown-item" href="#">Estoques</a>
+                        <a class="dropdown-item" href="#">Histórico</a>
                     </div>
                 </div>
                 </p>
@@ -122,21 +114,12 @@
         <div class="card h-100">
             <div class="bg-holder d-none d-xl-block bg-card" style="background-image:url(<?= base_url() ?>/public/v2/assets/img/illustrations/corner-4.pngXXX);"></div>
             <div class="card-body">
-                <h6>Whatsapp</h6>
+                <h6>API Whatsapp</h6>
                 <div><strong class="mr-2">Status: </strong>
                     <div class="badge rounded-pill badge-soft-success fs--2">Operando<span class="fas fa-check ml-1" data-fa-transform="shrink-2"></span></div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-sm-12 mb-3">
-        <div class="card h-100">
-            <div class="bg-holder d-none d-xl-block bg-card" style="background-image:url(<?= base_url() ?>/public/v2/assets/img/illustrations/corner-4.pngXXX);"></div>
-            <div class="card-body">
-                <h6>Backup automático</h6>
-                <div><strong class="mr-2">Realizado: </strong>
-                    <div class="badge rounded-pill badge-soft-success fs--2"><?= date('d/m/Y') ?><span class="fas fa-check ml-1" data-fa-transform="shrink-2"></span></div>
-                </div>
+                <hr>
+                <small class="text-danger"><i class="fas fa-info-circle"></i> A API do Whatsapp é responsável pelo envio de mensagens aos pacientes e usuários do sistema.</small>
             </div>
         </div>
     </div>
@@ -148,7 +131,23 @@
                 <div><strong class="mr-2">Status: </strong>
                     <div class="badge rounded-pill badge-soft-success fs--2">Operando<span class="fas fa-check ml-1" data-fa-transform="shrink-2"></span></div>
                 </div>
+                <hr>
+                <small class="text-danger"><i class="fas fa-info-circle"></i> API de dados é responsável pela integridade dos dados fornecidos pelo sistema.</small>
             </div>
         </div>
     </div>
+    <div class="col-lg-4 col-sm-12 mb-3">
+        <div class="card h-100">
+            <div class="bg-holder d-none d-xl-block bg-card" style="background-image:url(<?= base_url() ?>/public/v2/assets/img/illustrations/corner-4.pngXXX);"></div>
+            <div class="card-body">
+                <h6>Backup automático</h6>
+                <div><strong class="mr-2">Realizado: </strong>
+                    <div class="badge rounded-pill badge-soft-success fs--2"><?= date('d/m/Y') ?><span class="fas fa-check ml-1" data-fa-transform="shrink-2"></span></div>
+                </div>
+                <hr>
+                <small class="text-danger"><i class="fas fa-info-circle"></i> Backups do banco de dados são realizados constantemente para segurança do município.</small>
+            </div>
+        </div>
+    </div>
+
 </div>
